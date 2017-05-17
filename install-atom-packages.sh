5553e1e6a0dd0ce66e9efe5fa853b69c0927b6ef
Miles Johnson <mileswjohnson@gmail.com>
2017-05-16 19:33:00 -0700

    Updated atom config.

--- install-atom-packages.sh
+++ install-atom-packages.sh
@@ -3,13 +3,15 @@
 echo -e "\033[32mInstalling Atom packages\033[0m"
 
 apm install atom-beautify
+apm install autocomplete-modules
 apm install caniuse
+apm install copy-path
 apm install file-icons
 apm install fonts
-apm install js-hyperclick
 apm install highlight-selected
 apm install hyperclick
 apm install hyperclick-php
+apm install js-hyperclick
 apm install language-babel
 apm install language-graphql
 apm install language-ini
@@ -17,11 +19,14 @@ apm install language-php
 apm install language-rust
 apm install linter-eslint
 apm install linter-php
+apm install linter-ruby
 apm install linter-rust
 apm install linter-sass-lint
 apm install nuclide
 apm install nuclide-format-js
+apm install related
 apm install sort-lines
 apm install tool-bar
+apm install tree-view-copy-relative-path
 
 echo -e "\n\033[32mComplete!\033[0m"
