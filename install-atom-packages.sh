c399f6d8c5ed7ca8079457f363dfc69ab5b1189d
Miles Johnson <mileswjohnson@gmail.com>
2017-09-03 21:46:04 -0700

    Updated packages.

--- install-atom-packages.sh
+++ install-atom-packages.sh
@@ -3,6 +3,7 @@
 echo -e "\033[32mInstalling Atom packages\033[0m"
 
 apm install atom-beautify
+apm install atom-typescript
 apm install autocomplete-modules
 apm install caniuse
 apm install copy-path
@@ -11,16 +12,19 @@ apm install fonts
 apm install highlight-selected
 apm install hyperclick
 apm install hyperclick-php
+apm install intentions
 apm install js-hyperclick
 apm install language-babel
 apm install language-graphql
 apm install language-ini
 apm install language-rust
 apm install linter-eslint
+apm install linter-markdown
 apm install linter-php
 apm install linter-ruby
 apm install linter-rust
 apm install linter-sass-lint
+apm install merge-conflicts
 apm install nuclide
 apm install nuclide-format-js
 apm install related
