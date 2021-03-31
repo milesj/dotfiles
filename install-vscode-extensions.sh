b1663158714b957f34391d16d6be39fccb50b641
Miles Johnson <>
2021-03-31 10:28:45 -0700

    Update vscode config.

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -3,6 +3,8 @@ echo -e "\033[32mInstalling VSCode extensions\033[0m"
 code --install-extension agauniyal.vscode-caniuse
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension digitalbrainstem.javascript-ejs-support
+code --install-extension editorconfig.editorconfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
@@ -11,6 +13,7 @@ code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode.atom-keybindings
 code --install-extension naumovs.color-highlight
 code --install-extension npxms.hide-gitignored
-code --install-extension PKief.material-icon-theme
+code --install-extension pkief.material-icon-theme
 code --install-extension qassimfarid.ejs-language-support
+code --install-extension silvenon.mdx
 code --install-extension wayou.vscode-todo-highlight
