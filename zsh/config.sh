15120869eb65f29cd7ba203877fb0bfcae2be87d
Miles Johnson <mileswjohnson@gmail.com>
2023-01-07 17:48:51 -0800

    Update more configs.

--- zsh/config.sh
+++ zsh/config.sh
@@ -30,8 +30,8 @@ eval "$(scmpuff init -s --aliases=false)"
 
 # Setup nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Source aliases and exports
 source "$ZSH/oh-my-zsh.sh"
